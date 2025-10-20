da57f994a935c32c0b8206ef0cf2010d49e43bc0
Yaroslav Chertov <yachertov@yandex.ru>
2025-10-20 22:52:11 +0300

    add adaptive for 1160

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector('.header__burger');
+const menu = document.querySelector('.header__menu');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    menu.classList.toggle('header__menu--open');
+});
