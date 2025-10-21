260b622f2d6ca325f54198685e6cc1b0cc839658
Yaroslav Chertov <yachertov@yandex.ru>
2025-10-21 17:50:41 +0300

    add adaptive 576 & readme

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,34 @@
-const burger = document.querySelector('.header__burger');
-const menu = document.querySelector('.header__menu');
+document.addEventListener('DOMContentLoaded', () => {
+    // Бургер меню
+    const burger = document.querySelector('.header__burger');
+    const menu = document.querySelector('.header__menu');
 
-burger.addEventListener('click', () => {
-    burger.classList.toggle('active');
-    menu.classList.toggle('header__menu--open');
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('active');
+        menu.classList.toggle('header__menu--open');
+    });
+
+    // Показать все карточки
+    const cards = document.querySelectorAll('.equipment__card');
+    const showMoreBtn = document.querySelector('.equipment__catalog--btn');
+
+    const hideExtraCards = () => {
+        if (window.innerWidth <= 576) {
+            cards.forEach((card, index) => {
+                if (index >= 4) card.style.display = 'none';
+            });
+        } else {
+            cards.forEach((card) => (card.style.display = 'flex'));
+        }
+    };
+
+    hideExtraCards();
+    window.addEventListener('resize', hideExtraCards);
+
+    showMoreBtn.addEventListener('click', () => {
+        if (window.innerWidth <= 576) {
+            cards.forEach((card) => (card.style.display = 'flex'));
+            showMoreBtn.disabled = true;
+        }
+    });
 });
